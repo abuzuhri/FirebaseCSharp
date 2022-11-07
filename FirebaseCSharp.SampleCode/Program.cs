e204f5ad275eacae1b1936065ce7c4f1f4d5d957
Tareq Abuzuhri <tareq_abuzuhri@hotmail.com>
2022-11-07 23:51:06 +0100

    add sample

---
+++ FirebaseCSharp.SampleCode/Program.cs
@@ -0,0 +1,25 @@
+﻿using FirebaseCSharp;
+using FirebaseCSharp.Config;
+using FirebaseCSharp.Interfaces;
+using FirebaseCSharp.Response;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+
+var configuration = new ConfigurationBuilder()
+.AddUserSecrets<Program>()
+.Build();
+
+
+
+IFirebaseConfig config = new FirebaseConfig
+{
+    AuthSecret = configuration.GetSection("Firebase:AuthSecret").Value,
+    BasePath = configuration.GetSection("Firebase:BasePath").Value
+};
+
+IFirebaseClient client = new FirebaseClient(config);
+
+FirebaseResponse response = client.Get("Inventory");
+var d = JsonConvert.DeserializeObject<Dictionary<string, string>>(response.Body);
+
